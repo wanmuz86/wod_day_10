c4e9fe7bfd8b283cf419c7c396d9da77b58e87e2
Wan Muzaffar <wanmuz86@gmail.com>
2017-08-08 19:17:23 +0800

    My feedback

--- wod_10.js
+++ wod_10.js
@@ -1,5 +1,7 @@
 var arr = [3,12,5,7,151,73,155];
 	for(i = 0; i < arr.length; i++){
+		
+		//This is not a O(/n) solution.. You making it worst to become O(n2) :(
 		var isPrime = true;
 		for(j = 2; j < arr[i]; j++){
 			if(arr[i] % j === 0){
