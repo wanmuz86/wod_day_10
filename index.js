822a052dcdd1467ec432f72349855d843925f2b6
Wan Muzaffar <wanmuz86@gmail.com>
2017-08-08 13:35:23 +0800

    My feedback
    My feedback

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function isPrime(input){
     document.write(input[j]+" ");
  
     for(var i=2; i<=Math.sqrt(input[j]); i++){
-   
+   // Currently your code is O(/n) .. Eg: If the number is 100, it will take 10 round to do verification..
+	    
+	    //Check if you can revise it so that only take half round to do it..
     if (input[j]%i == 0)
       
       prime = false;
